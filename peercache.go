f8b95efd0b0d24ce7ec8aadda5e8cdfc18649dbd
Jeff Chao <jeffchao@me.com>
2014-07-29 22:36:46 -0700

    Stub out peercache file.

---
+++ peercache.go
@@ -0,0 +1,9 @@
+/*
+Package main is a reference to the binary it produces, peercache.
+Peercache is a fault-tolerant distributed cache designed to handle
+hotspots and dynamic cluster sizes.
+*/
+package main
+
+func main() {
+}
